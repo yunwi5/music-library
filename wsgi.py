10ae0c6798d230a3705c1124b450259e53c51954
Yunkeun <1009jyk@gmail.com>
2022-11-11 02:13:22 +1300

    Change wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,9 @@
 """App entry point."""
 from music import create_app
-from os import environ
 
 app = create_app()
 
-if __name__ == "__main__":
-    port = environ.get('PORT')
-    print(f"port: {port}")
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == "__main__":
+    # port = environ.get('PORT')
+    # print(f"port: {port}")
+    # app.run(host='0.0.0.0', port=5000)
